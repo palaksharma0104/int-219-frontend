3d92beea29c49d80c436954cafb2f7176712e592
Palak Sharma <117135999+palaksharma0104@users.noreply.github.com>
2024-02-29 13:38:29 +0530

    To check the number of vowels in a string using js

---
+++ vowelsstring.js
@@ -0,0 +1,10 @@
+// Define a function named vowel_Count with parameter str
+function vowel_Count(str) {
+  // Use regular expression to replace all characters not in 'aeiou' with an empty string
+  // and get the length of the resulting string, which is the count of vowels
+  return str.replace(/[^aeiou]/g, "").length;
+}
+
+// Log the result of calling vowel_Count with the given strings to the console
+console.log(vowel_Count("Python"));
+console.log(vowel_Count("w3resource.com")); 
