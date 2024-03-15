2e6717b8c96006569373c63266bdd765de87c062
Palak Sharma <117135999+palaksharma0104@users.noreply.github.com>
2024-03-15 14:01:46 +0530

    Creating a new string from the previous string using js

---
+++ AddFrontBack.js
@@ -0,0 +1,16 @@
+// Define a function named front_back that takes a parameter str
+function front_back(str) {
+  // Extract the first character of str
+  first = str.substring(0, 1);
+  // Return the concatenation of first, str, and first
+  return first + str + first;
+}
+
+// Log the result of calling the front_back function with the argument 'a' to the console
+console.log(front_back('a'));
+
+// Log the result of calling the front_back function with the argument 'ab' to the console
+console.log(front_back('ab'));
+
+// Log the result of calling the front_back function with the argument 'abc' to the console
+console.log(front_back('abc')); 
